6c659770bbe1e81b7e4056da8213b4b6ded4401c
oaheux <145021731+oaheux@users.noreply.github.com>
2023-09-24 15:43:37 +0800

    Update trap.c

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -161,7 +161,7 @@ void exception_handler(struct trapframe *tf) {
             */
             cprintf("Exception type:Illegal instruction\n");
              cprintf("Illegal instruction caught at 0x%016llx\n", tf->epc);
-              tf->epc += 2;     //更新 tf->epc寄存器,机器模式异常返回指令mret是32位指令，长4字节
+              tf->epc += 2;     //更新 tf->epc寄存器
             break;
         case CAUSE_BREAKPOINT:
             //断点异常处理
@@ -170,9 +170,9 @@ void exception_handler(struct trapframe *tf) {
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
-             cprintf("Exception type:Illegal instruction\n");
-             cprintf("Illegal instruction caught at 0x%016llx\n", tf->epc);
-              tf->epc += 2;     //更新 tf->epc寄存器,机器模式异常返回指令mret是32位指令，长4字节
+             cprintf("Exception type:breakpoint\n");
+             cprintf("ebreak caught at 0x%016llx\n", tf->epc);
+              tf->epc += 2;     //更新 tf->epc寄存器
             break;
         case CAUSE_MISALIGNED_LOAD:
             break;
